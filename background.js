07337664f0d4484eeb6c1e9362a8fe35614b8118
Robert Breetzmann <robert.breetzman@gmail.com>
2012-04-21 17:46:56 +0200

    Added support for removing tabs

--- background.js
+++ background.js
@@ -1,28 +1,32 @@
 /**
- * Background functions.
- * Robert Breetzmann (robert.breetzmann@gmail.com) 
- * 
- * TODO: Removing activeOptions on removing tabs/windows
+ * Background functions. Robert Breetzmann (robert.breetzmann@gmail.com)
  */
-
 var Http = new function() {
-    this.get = function( url, cookies, callback ) {
-        var header = [{'key' : 'User-agent' , 'value' : 'Mozilla/4.0 (compatible)' }];
-        
+    this.get = function(url, cookies, callback) {
+        var header = [ {
+            'key' : 'User-agent',
+            'value' : 'Mozilla/4.0 (compatible)'
+        } ];
+
         var cookie = '';
-        for( var i=0;i<cookies.length;i++) {
+        for ( var i = 0; i < cookies.length; i++) {
             cookie += cookies[i].name + "=" + cookies[i].value + ";";
         }
-        
-        header.push( {'key' : 'Cookie' , 'value' : cookie } );
-        
-        var request = {'header' : header, 'url': url };
-      
+
+        header.push({
+            'key' : 'Cookie',
+            'value' : cookie
+        });
+
+        var request = {
+            'header' : header,
+            'url' : url
+        };
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function(data) {
             if (xhr.readyState == 4) {
                 if (xhr.status == 201 || xhr.status == 200) {
-                    if ( callback ) {
+                    if (callback) {
                         callback(xhr.responseText);
                     }
                     return;
@@ -43,87 +47,135 @@ var Http = new function() {
 }
 
 var SessionLiveSaver = new function() {
-    
+
     var activePings = new Array();
-    
+
     this.init = function() {
-        chrome.tabs.onCreated.addListener( function(input) {
-            
-            var options = JSON.parse( localStorage["sessionlifesaver"] );
-            
-            chrome.windows.getAll({populate: true}, function( windows ) {
-                for( var j=0;j<windows.length;j++) {
-                    var window = windows[j];
-                    for( var i=0;i<window.tabs.length;i++) {
-                        var tab = window.tabs[i];
-                        var option = getOption( tab.url, options );
-                        if( option == null ) {
-                            continue; 
-                        }
-                        addActiveOption( option ); 
-                    }
-                }
-            });
-            
+        chrome.tabs.onCreated.addListener(function(input) {
+            if (console) {
+                console.info('Tab added');
+            }
+            checkingOptions();
+        });
+
+        chrome.tabs.onRemoved.addListener(function(input) {
+            if (console) {
+                console.info('Tab removed');
+            }
+            checkingOptions();
         });
 
         run();
     }
-    
-    var addActiveOption = function( option ) {
-        var newOption = getOption( option.trigger, activePings);
-        
-        if( !newOption) {
+
+    var checkingOptions = function() {
+        chrome.windows.getAll({populate : true}, function(windows) {
+            checkingNewOptions(windows);
+            checkingInactiveOptions(windows);
+            if (console) {
+                console.info('Active queue: '+ JSON.stringify(activePings));
+            }
+        });
+    }
+
+    var checkingInactiveOptions = function(windows) {
+        if (console) {
+            console.info('Checking for inactive options');
+        }
+        for ( var i = 0; i < activePings.length; i++) {
+            var activePing = activePings[i];
+            var found = false;
+            for ( var k = 0; k < windows.length; k++) {
+                var window = windows[k];
+                for ( var j = 0; j < window.tabs.length; j++) {
+                    var tab = window.tabs[j];
+                    if (tab.url.indexOf(activePing.trigger) > -1) {
+                        found = true;
+                    }
+                }
+            }
+
+            if (!found) {
+                if (console) {
+                    console.info('Removed option from queue: ' + activePing.trigger);
+                }
+                activePings.splice(i, 1);
+            }
+        }
+    }
+
+    var checkingNewOptions = function(windows) {
+        if (console) {
+            console.info('Checking all tabs for new options');
+        }
+        for ( var k = 0; k < windows.length; k++) {
+            var window = windows[k];
+            for ( var i = 0; i < window.tabs.length; i++) {
+                var tab = window.tabs[i];
+                var option = getOption(tab.url, JSON.parse(localStorage["sessionlifesaver"]));
+                if (option == null) {
+                    continue;
+                }
+                addActiveOption(option);
+            }
+        }
+    }
+
+    var addActiveOption = function(option) {
+        var newOption = getOption(option.trigger, activePings);
+
+        if (!newOption) {
             option.count = 1;
-            activePings.push( option ); 
+            activePings.push(option);
             if (console) {
                 console.info('Added option to queue: ' + option.trigger);
             }
         } else {
             if (console) {
-                console.info('Option: ' + option.trigger + ' already in queue.' );
+                console.info('Option: ' + option.trigger + ' already in queue.');
             }
         }
     }
-    
-    
-    var getOption = function( url, options ) {
-        for( var i=0;i<options.length;i++) {
-           if( url.indexOf( options[i].trigger ) > -1 ) {
-             return options[i]; 
-           }
+
+    var getOption = function(url, options) {
+        for ( var i = 0; i < options.length; i++) {
+            if (url.indexOf(options[i].trigger) > -1) {
+                return options[i];
+            }
         }
 
         return null;
     }
-    
-    var ping = function( option ) {
-        chrome.cookies.getAll({ url: option.ping }, function( cookies ) {
-            Http.get( option.ping, cookies );
-            if( console ) {
-                console.info( "Ping " + option.ping );
-                console.info( "Next ping in " + option.time + " minutes" );
+
+    var ping = function(option) {
+        chrome.cookies.getAll({
+            url : option.ping
+        }, function(cookies) {
+            Http.get(option.ping, cookies);
+            if (console) {
+                console.info("Ping " + option.ping);
+                console.info("Next ping in " + option.time + " minutes");
             }
-        }); 
+        });
     }
 
     var run = function() {
-        
+
         setTimeout(function() {
-            if( console ) {
-                console.info( "Checking now"); 
+            if (console) {
+                console.info("Checking now");
             }
-            for( var i=0;i<activePings.length;i++) {
-                if( parseInt(activePings[i].time) == activePings[i].count) {
+            for ( var i = 0; i < activePings.length; i++) {
+                if (parseInt(activePings[i].time) == activePings[i].count) {
                     activePings[i].count = 1;
-                    ping( activePings[i] );
+                    ping(activePings[i]);
                 } else {
                     activePings[i].count += 1;
                 }
-                
+
             }
             run();
-        }, 1000*60);
+        }, 1000 * 60);
 
     }
 }
